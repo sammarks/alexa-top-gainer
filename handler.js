531284764c560e5fbdda51fcc9d1abe7c9560d3a
Sam Marks <sam@sammarks.me>
2016-12-18 21:45:43 -0500

    Make it work.

--- handler.js
+++ handler.js
@@ -1,40 +1,42 @@
 'use strict';
-import jsdom from 'jsdom'
+const jsdom = require('jsdom').jsdom
+const rp = require('request-promise')
+const q = require('q')
 
 module.exports.topGainer = (event, context, callback) => {
-  jsdom.env(
-    'https://finance.yahoo.com/gainers',
-    (err, window) => {
 
-      const response = {
-        version: '1.0',
-        response: {
-          outputSpeech: {
-            type: 'PlainText',
-            text: ''
-          }
-        }
+  const response = {
+    version: '1.0',
+    response: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: ''
       }
-
-      let error = null
-      if (err) {
-        error = err
-      } else {
-        try {
-          const name = window.App.main.context.dispatcher.stores.YFinListStore.lists.gainers.positions[0].name
-          response.response.outputSpeech.text = `The top gainer for today is ${name}`
-        } catch (e) {
-          error = e
-        }
-      }
-
-      if (error) {
-        console.error(error)
-        response.response.outputSpeech.text = 'Sorry, there was an error fetching the data.'
-      }
-
-      callback(null, response)
-
     }
-  )
+  }
+
+  rp({
+    method: 'GET',
+    url: 'https://finance.yahoo.com/gainers'
+  }).then((body) => {
+    const promise = q.defer()
+    const doc = jsdom(body)
+    doc.addEventListener('load', () => {
+      try {
+        const window = doc.defaultView
+        const name = window.App.main.context.dispatcher.stores.YFinListStore.lists.gainers.positions[0].name
+        response.response.outputSpeech.text = 'The top gainer for today is ' + name
+        callback(null, response)
+        promise.resolve()
+      } catch (err) {
+        promise.reject(err)
+      }
+    })
+    return promise
+  }).catch((err) => {
+    console.error(err)
+    response.response.outputSpeech.text = 'Sorry, there was an error fetching the data.'
+    callback(null, response)
+  })
+
 }
