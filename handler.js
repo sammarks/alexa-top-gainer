bd205b939ef474b2c22e10c486fbd375e7ea40a9
Sam Marks <sam@sammarks.me>
2016-12-18 20:59:09 -0500

    Call Yahoo Finance API to get the top gainer.

--- handler.js
+++ handler.js
@@ -1,15 +1,40 @@
 'use strict';
+import jsdom from 'jsdom'
 
 module.exports.topGainer = (event, context, callback) => {
-  const response = {
-    version: '1.0',
-    response: {
-      outputSpeech: {
-        type: 'PlainText',
-        text: 'Here\'s some text.'
-      }
-    }
-  }
+  jsdom.env(
+    'https://finance.yahoo.com/gainers',
+    (err, window) => {
 
-  callback(null, response)
+      const response = {
+        version: '1.0',
+        response: {
+          outputSpeech: {
+            type: 'PlainText',
+            text: ''
+          }
+        }
+      }
+
+      let error = null
+      if (err) {
+        error = err
+      } else {
+        try {
+          const name = window.App.main.context.dispatcher.stores.YFinListStore.lists.gainers.positions[0].name
+          response.response.outputSpeech.text = `The top gainer for today is ${name}`
+        } catch (e) {
+          error = e
+        }
+      }
+
+      if (error) {
+        console.error(error)
+        response.response.outputSpeech.text = 'Sorry, there was an error fetching the data.'
+      }
+
+      callback(null, response)
+
+    }
+  )
 }
